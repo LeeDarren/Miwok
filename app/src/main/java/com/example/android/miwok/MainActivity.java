ef72e4029334a155f69e768a5550bd5c10b7204b
Darren Lee <dtg472@gmail.com>
2017-01-17 22:42:58 -0800

    Add the inefficient way for practice, Lesson 1 "How to create an event listener"

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,18 +19,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+        //Testing a click listener with a complete other class
+        NumbersClickListener clickListener = new NumbersClickListener();
+        //Find the veiw that shows the numbers category
+        TextView numbers = (TextView)findViewById(R.id.numbers);
+        numbers.setOnClickListener(clickListener);
     }
 
-    public void openNumbersList(View view){
+    public void openNumbersList(View view) {
         Intent intent = new Intent(this, NumbersActivity.class);
         startActivity(intent);
     }
