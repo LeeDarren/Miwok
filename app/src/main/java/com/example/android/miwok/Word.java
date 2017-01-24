10a301241f1952ae8b432a8302e9f0f29ab14c22
Darren Lee <dtg472@gmail.com>
2017-01-24 00:48:39 -0800

    Update to Lesson 4: Play Correct Audio File Per Word

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -11,26 +11,46 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
-    private int mImageResourceID=NO_IMAGE_PROVIDED;
+    private int mImageResourceID = NO_IMAGE_PROVIDED;
+    private int mAudioResourceID = NO_AUDIO_PROVIDED;
 
     private static final int NO_IMAGE_PROVIDED = -1;
+    private static final int NO_AUDIO_PROVIDED = -1;
 
-    /** basic public constructor, with word pair
+    /**
+     * basic public constructor, with word pair
+     *
      * @param defaultTranslation word in default language
-     * @param miwokTranslation word in miwok
+     * @param miwokTranslation   word in miwok
      */
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
-    /** Constructor, with word pair and imageID
+    /**
+     * Constructor, with word pair and imageID
+     *
      * @param defaultTranslation word in default language
-     * @param miwokTranslation word in miwok
-     * @param imageResourceID icon image for word
+     * @param miwokTranslation   word in miwok
+     * @param imageResourceID    icon image for word
+     * @param audioResourceID    audio file for word
      */
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID){
-        this(defaultTranslation,miwokTranslation);
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID, int audioResourceID) {
+        this(defaultTranslation, miwokTranslation);
         mImageResourceID = imageResourceID;
+        mAudioResourceID = audioResourceID;
+    }
+
+    /**
+     * Constructor, with word pair and imageID
+     *
+     * @param defaultTranslation word in default language
+     * @param miwokTranslation   word in miwok
+     * @param audioResourceID    audio file for word
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceID) {
+        this(defaultTranslation, miwokTranslation);
+        mAudioResourceID = audioResourceID;
     }
     /**
      * Get default translation
@@ -49,8 +69,19 @@ public class Word {
     /*
      * Get resource id of image
      */
-    public int getImageResourceID() { return mImageResourceID; }
+    public int getImageResourceID() {
+        return mImageResourceID;
+    }
+
+    /*
+     * Get resource id of audio
+     */
+    public int getAudioResourceID() {
+        return mAudioResourceID;
+    }
 
     //Returns true if there is an image
-    public boolean hasImage() { return mImageResourceID != NO_IMAGE_PROVIDED; }
+    public boolean hasImage() {
+        return mImageResourceID != NO_IMAGE_PROVIDED;
+    }
 }
